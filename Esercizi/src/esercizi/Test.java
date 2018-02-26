fc51b1245a87de45295894d99493cbd74587072a
Nicola Zorloni <Nicola Zorloni@DESKTOP-TR5P0JM.supsi.ch>
2018-02-26 09:54:39 +0100

    da bruciare

---
+++ Esercizi/src/esercizi/Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercizi;
+
+/**
+ *
+ * @author Nicola Zorloni
+ */
+public class Test extends Esercizi{
+
+    public Test() {
+        for (int i = 0; i < 10; i++) {
+            while (true) {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+    
+}
