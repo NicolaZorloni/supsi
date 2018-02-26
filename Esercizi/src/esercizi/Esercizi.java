c8d8c3b2c465f54ceb7a1dc4ac57a9c2c7431d63
Nicola Zorloni <Nicola Zorloni@DESKTOP-TR5P0JM.supsi.ch>
2018-02-26 09:23:09 +0100

    test02

--- Esercizi/src/esercizi/Esercizi.java
+++ Esercizi/src/esercizi/Esercizi.java
@@ -14,8 +14,5 @@ public class Esercizi {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
