5d40f03ce561c4eb6e8842005e049e65d14597d8
Nicola Zorloni <Nicola Zorloni@DESKTOP-TR5P0JM.supsi.ch>
2018-02-26 09:54:25 +0100

    da bruciare

--- Esercizi/src/esercizi/Esercizi.java
+++ Esercizi/src/esercizi/Esercizi.java
@@ -10,9 +10,7 @@ package esercizi;
  * @author Nicola Zorloni
  */
 public class Esercizi {
-
-    /**
-     * @param args the command line arguments
-     */
+    public static void main(String[] args) {
+    }
     
 }
